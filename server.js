ed0174f04d77f6d0acf935da379f3be37bb04a44
Pimser <142997610+Pimser@users.noreply.github.com>
2025-03-17 11:06:06 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,28 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 3000;
+
+
+app.set("view engine", "ejs");
+
+const PORT = process.env.PORT || 3000;
+const dbURI = process.env.MONGO_URI;
+
+
+app.use(express.static("public"));
+
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+
+app.listen(PORT, () => {
+  console.log("Serveren kjører på port 3000!");
+});
